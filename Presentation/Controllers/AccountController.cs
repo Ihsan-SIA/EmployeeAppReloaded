9323ae9ef752fe8108fcbace9313a5bde4aecc62
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-06-27 02:50:17 +0100

    Trying to implement edit profile logic

--- Presentation/Controllers/AccountController.cs
+++ Presentation/Controllers/AccountController.cs
@@ -81,6 +81,13 @@ namespace Presentation.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+
+
+
+
+
+
+
         [HttpGet]
         public async Task<IActionResult> EditProfile()
         {
@@ -90,8 +97,13 @@ namespace Presentation.Controllers
                 SetFlashMessage("Sorry could not find user!", "error");
                 return RedirectToAction("Index");
             }
-
-            return View(user);
+            var model = new EditProfileViewModel
+            {
+                Id = user.Id,
+                Email = user.Email,
+                UserName = user.UserName
+            };
+            return View(model);
         }
 
         [HttpPost]
@@ -99,118 +111,123 @@ namespace Presentation.Controllers
         {
             if (!ModelState.IsValid)
             {
-                TempData["ErrorMessage"] = "Incorrect input of Email";
+                SetFlashMessage("Invalid input! Check again", "error");
                 return View(model);
             }
 
-
-            var user = await _userManager.GetUserAsync(User);
+            var user = await _userManager.FindByIdAsync(model.Id);
             if (user is null)
             {
-                TempData["ErrorMessage"] = "Failed to update";
+                SetFlashMessage("Sorry user does not exist", "error");
                 return RedirectToAction("Index");
             }
 
             user.Email = model.Email;
+            user.UserName = model.UserName;
             await _userManager.UpdateAsync(user);
             return RedirectToAction("Index", "Home");
         }
-        [AllowAnonymous]
-        public async Task<IActionResult> ConfirmEmail(string userId, string token)
-        {
-            if (userId == null || token == null) return RedirectToAction("Index", "Home");
-            var user = await _userManager.FindByIdAsync(userId);
-            if (user == null) return NotFound();
-            var result = await _userManager.ConfirmEmailAsync(user, token);
-            return View(result.Succeeded ? "ConfirmEmail" : "Error");
-        }
-
-        public IActionResult ResetPassword(string token, string email)
-        {
-            if (string.IsNullOrEmpty(token) || string.IsNullOrEmpty(email))
-            {
-                return BadRequest("Invalid password reset token.");
-            }
-
-            var model = new ResetPasswordViewModel
-            {
-                Token = token,
-                Email = email
-            };
-
-            return View(model);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> ResetPassword(ResetPasswordViewModel model)
-        {
-            if (!ModelState.IsValid)
-                return View(model);
-
-            var user = await _userManager.FindByEmailAsync(model.Email);
-            if (user == null)
-            {
-                return RedirectToAction("ResetPasswordConfirmation");
-            }
-
-            var result = await _userManager.ResetPasswordAsync(user, model.Token, model.Password);
-            if (result.Succeeded)
-            {
-                return RedirectToAction("ResetPasswordConfirmation");
-            }
-
-            foreach (var error in result.Errors)
-            {
-                ModelState.AddModelError(string.Empty, error.Description);
-            }
-
-            return View(model);
-        }
-
-        public IActionResult ResetPasswordConfirmation()
-        {
-            return View();
-        }
 
 
 
-        [AllowAnonymous]
-        [HttpGet]
-        public IActionResult ForgotPassword()
-        {
-            return View();
-        }
 
-        [AllowAnonymous]
-        [HttpPost]
-        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
-        {
-            if (!ModelState.IsValid)
-                return View(model);
 
-            var user = await _userManager.FindByEmailAsync(model.Email);
-            if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
-            {
-                return RedirectToAction("ForgotPasswordConfirmation");
-            }
+        //[AllowAnonymous]
+        //public async Task<IActionResult> ConfirmEmail(string userId, string token)
+        //{
+        //    if (userId == null || token == null) return RedirectToAction("Index", "Home");
+        //    var user = await _userManager.FindByIdAsync(userId);
+        //    if (user == null) return NotFound();
+        //    var result = await _userManager.ConfirmEmailAsync(user, token);
+        //    return View(result.Succeeded ? "ConfirmEmail" : "Error");
+        //}
 
-            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
-            var callbackUrl = Url.Action("ResetPassword", "Account", new { token, email = model.Email }, Request.Scheme);
+        //public IActionResult ResetPassword(string token, string email)
+        //{
+        //    if (string.IsNullOrEmpty(token) || string.IsNullOrEmpty(email))
+        //    {
+        //        return BadRequest("Invalid password reset token.");
+        //    }
 
-            // Send the email using your EmailSender service
-            await _emailSender.SendEmailAsync(
-                model.Email,
-                "Reset Password",
-                $"Please reset your password by clicking <a href='{callbackUrl}'>here</a>.");
+        //    var model = new ResetPasswordViewModel
+        //    {
+        //        Token = token,
+        //        Email = email
+        //    };
 
-            return RedirectToAction("ForgotPasswordConfirmation");
-        }
+        //    return View(model);
+        //}
 
-        [AllowAnonymous]
-        public IActionResult ForgotPasswordConfirmation()
-        {
-            return View();
-        }
+        //[HttpPost]
+        //public async Task<IActionResult> ResetPassword(ResetPasswordViewModel model)
+        //{
+        //    if (!ModelState.IsValid)
+        //        return View(model);
+
+        //    var user = await _userManager.FindByEmailAsync(model.Email);
+        //    if (user == null)
+        //    {
+        //        return RedirectToAction("ResetPasswordConfirmation");
+        //    }
+
+        //    var result = await _userManager.ResetPasswordAsync(user, model.Token, model.Password);
+        //    if (result.Succeeded)
+        //    {
+        //        return RedirectToAction("ResetPasswordConfirmation");
+        //    }
+
+        //    foreach (var error in result.Errors)
+        //    {
+        //        ModelState.AddModelError(string.Empty, error.Description);
+        //    }
+
+        //    return View(model);
+        //}
+
+        //public IActionResult ResetPasswordConfirmation()
+        //{
+        //    return View();
+        //}
+
+
+
+        //[AllowAnonymous]
+        //[HttpGet]
+        //public IActionResult ForgotPassword()
+        //{
+        //    return View();
+        //}
+
+        //[AllowAnonymous]
+        //[HttpPost]
+        //public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
+        //{
+        //    if (!ModelState.IsValid)
+        //        return View(model);
+
+        //    var user = await _userManager.FindByEmailAsync(model.Email);
+        //    if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
+        //    {
+        //        return RedirectToAction("ForgotPasswordConfirmation");
+        //    }
+
+        //    var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+        //    var callbackUrl = Url.Action("ResetPassword", "Account", new { token, email = model.Email }, Request.Scheme);
+
+        //    // Send the email using your EmailSender service
+        //    await _emailSender.SendEmailAsync(
+        //        model.Email,
+        //        "Reset Password",
+        //        $"Please reset your password by clicking <a href='{callbackUrl}'>here</a>.");
+
+        //    return RedirectToAction("ForgotPasswordConfirmation");
+        //}
+
+        //[AllowAnonymous]
+        //public IActionResult ForgotPasswordConfirmation()
+        //{
+        //    return View();
+        //}
 
     }
 }
